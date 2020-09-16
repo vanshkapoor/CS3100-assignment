82a8fecf2507f15924cc4b36a8a001e8cd89de10
kaushalsharma1998 <kaushalsharma1998@gmail.com>
2020-09-16 14:03:09 +0530

    main update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "employee.cpp"
+//#include "employee.cpp"
 
 using namespace std;
 
+
+void read+rec()
+{
+  Employee e;
+  int id;
+  string fn,ln;
+  cout<<"Enter details of the employee: "<<endl;
+  cout<<"Enter firstname: "<<endl;
+  gets(fn);
+  cout<<"Enter lastname: "<<endl;
+  gets(fn);
+  cout<<"Enter ID: "<<endl;
+  cin>>id;
+  e.setFirstname(fn);
+  e.setLastname(ln);
+  e.setID(id);
+
+  cout<<"Your Details"<<endl;
+
+
+}
 int main()
 {
     // ------------------ MENU -----------------
@@ -12,6 +33,26 @@ int main()
     // (3) Read two employee records from the keyboard, and test if these two employees are the same.
     // (4) Read two employee records from the keyboard, and test if one employee’s ID is less than another employee’s ID
     // (5) Quit:  close all the files, and quit.
+    cout<<"------------------------------------Menu--------------------------------------------------------------------------------------------------------------------"<<endl;
+    cout<<"(1) Read an employee record from the keyboard, and display it on the screen, and write it to the output file simultaneously"<<endl;
+    cout<<"(2).Read a specified number of the employee records from the input file, and display them on the screen, and write them to the output file simultaneously."<<endl;
+    cout<<"(3) Read two employee records from the keyboard, and test if these two employees are the same."<<endl;
+    cout<<"(4) Read two employee records from the keyboard, and test if one employee’s ID is less than another employee’s ID"<<endl;
+    cout<<"(5) Quit:  close all the files, and quit"<<endl;
+    cout<<"PLEASE ENTER YOUR CHOICE"<<endl;
+    int choice c=0;
+    cin>>choice;
+
+    switch (choice) {
+      case 1 : read_rec();break;
+      case 2:  read_specified();break;
+      case 3:  read_2_equal(); break;
+      case 4:  read_2_compare();break;
+      case 5: break;
+      default: cout<<"Wrong Choice";
+      goto(a);
+    }
+
 
     return 0;
 }
