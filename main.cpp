6156ee62012b2d714c32fcf57ba692fdc57d054c
vanshkapoor <vanshkapoorvk7@gmail.com>
2020-09-16 18:18:06 +0530

    file read from main done

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "employee.cpp"
+#include <fstream>
+// #include "employee.cpp"
 
 using namespace std;
 
+void readFile()
+{
+  fstream file;
+  string word;
+  int cnt = 0;
+  string filename = "Small-Database.txt";
+  file.open(filename.c_str());
+  cout << endl;
+  cout << endl;
+  cout << "------ DATA BASE FILE ----" << endl;
+  cout << endl;
+  cout << endl;
+  cout << "FIRSTNAME  -  LASTNAME  - ID" << endl;
+  cout << endl;
+  while (file >> word)
+  {
+
+    cout << word << "     ";
+    cnt++;
+    if (cnt == 3)
+    {
+      cout << endl;
+      cnt = 0;
+    }
+  }
+}
+
 int main()
 {
-// ------------------ MENU -----------------
-// (1) Read an employee record from the keyboard, and display it on the screen, and write it to the output file simultaneously.
-// (2). Read a specified number of the employee records from the input file, and display them on the screen, and write them to the output file simultaneously.
-// (3) Read two employee records from the keyboard, and test if these two employees are the same.
-// (4) Read two employee records from the keyboard, and test if one employee’s ID is less than another employee’s ID
-// (5) Quit:  close all the files, and quit.
+  // STEP 1 : READ INPUT FILE AND DISPLAY IT
+  readFile();
+  cout << endl;
+  cout << endl;
 
   cout << "------------------------------------Menu--------------------------------------------------------------------------------------------------------------------" << endl;
   cout << "(1) Read an employee record from the keyboard, and display it on the screen, and write it to the output file simultaneously" << endl;
@@ -26,11 +52,11 @@ int main()
   switch (choice)
   {
   case 1:
-    checklink();
+    // checklink();
     // read_rec();
     break;
   case 2:
-    read_specified();
+    // read_specified();
     break;
   // case 3:
   //   read_2_equal();
